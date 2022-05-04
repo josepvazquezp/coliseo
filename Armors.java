1498dd1ec2b48f2713d77c897c6db70cad49fb8b
josepvazquezp <jose.vazquez@iteso.mx>
2022-05-04 17:24:33 -0500

    primeras clases

---
+++ Armors.java
@@ -0,0 +1,25 @@
+package com.iteso.motor;
+
+public enum Armors {
+	GOLD_HELMET(150, 50, true, "gold helmet", "helmet/armor", Material.GOLD), 
+	IRON_HELMET(100, 30, true, "iron helmet", "helmet/armor", Material.IRON),  
+	COPPER_HELMET(50, 20, true, "copper helmet", "helmet/armor", Material.COPPER), 
+	GOLD_BREASTPLATE(300, 100, false, "gold breastplate", "breastplate/armor", Material.GOLD), 
+	IRON_BREASTPLATE(200, 80, false, "gold breastplate", "breastplate/armor", Material.IRON), 
+	COPPER_BREASTPLATE(100, 60, false, "gold breastplate", "breastplate/armor", Material.COPPER);
+	
+	private Armor armor;
+	
+	private Armors(int defense, int weight, boolean helmet, String name, String category, Material type) {
+		armor.setDefense(defense);
+		armor.setWeight(weight);
+		armor.setHelmet(helmet);
+		armor.setName(name);
+		armor.setCategory(category);
+		armor.setType(type);
+	}
+	
+	public Armor getArmor() {
+		return this.armor;
+	}
+}
