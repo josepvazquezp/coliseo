5cf97b7e17c309c300272645a0ad057395e6cee5
José Pablo Vázquez Partida <74749686+josepvazquezp@users.noreply.github.com>
2022-05-05 22:11:21 -0500

    hitbox posiblemente lista

--- Hitbox.java
+++ Hitbox.java
@@ -2,14 +2,11 @@ package com.iteso.motor;
 
 public class Hitbox {
 	
-	private Role character;
-	private Weapon weapon;
-	
-	public boolean hit(int long1, int height1, int long2, int height2) {
-		double minXRole = character.getX() - long1 / 2.0;
-		double maxXRole = character.getX() + long1 / 2.0;
-		double minYRole = character.getY() - height1 / 2.0;
-		double maxYRole = character.getY() + height1 / 2.0;
+	public static boolean hit(Role rol, int long1, int height1, Weapon weapon, int long2, int height2) {
+		double minXRole = rol.getX() - long1 / 2.0;
+		double maxXRole = rol.getX() + long1 / 2.0;
+		double minYRole = rol.getY() - height1 / 2.0;
+		double maxYRole = rol.getY() + height1 / 2.0;
 		double minXWeapon = weapon.getX() - long2 / 2.0;
 		double maxXWeapon = weapon.getX() - long2 / 2.0;
 		double minYWeapon = weapon.getY() - height2 / 2.0;
@@ -19,7 +16,7 @@ public class Hitbox {
 		   ((minYWeapon >= minYRole && minYWeapon <= maxYRole) | (maxYWeapon >= minYRole && maxYWeapon <= maxYRole)))
 			return true;
 		
-		
 		return false;
 	}
+
 }
