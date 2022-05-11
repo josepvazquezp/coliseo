98f33ceabe5d9d2a44e72493a7bf1d662ed24419
PP-rez <101601926+PP-rez@users.noreply.github.com>
2022-05-10 21:34:01 -0500

    documentacion hecha

--- Material.java
+++ Material.java
@@ -1,7 +1,11 @@
 package com.iteso.motor;
 
 import java.util.TreeMap;
-
+/**
+ * Clase Armor de la aplicación
+ * @author José Pablo Vázquez Partida, Octavio Valdez Fonseca, José Eduardo Pérez Valenzuela
+ * @version 1.0
+ */
 public class Material {
 	private String name;
 	private int endurance;
@@ -10,15 +14,25 @@ public class Material {
 	static {
 		data = new TreeMap<String, Integer>();
 	}
-	
+	/**
+	 * Método constructor por defecto
+	 */
 	public Material() {
 		
 	}
-	
+	/** 
+	 * Método constructor parametrizado
+	 * @param name Nombre del objeto del material
+	 * @param endurance Resistencia del objeto del material
+	 */
 	public Material(String name, int endurance) {
 		this.setMaterial(name, endurance);
 	}
-	
+	/**
+	 * Establece el material del objeto o si ya existe
+	 * @param name Nombre del material que se le asignara al objeto
+	 * @param endurance Resistencia del material que se le asignara al objeto
+	 */
 	public void setMaterial(String name, int endurance) {
 		if(Material.data.get(name) == null) {
 			this.name = name;
@@ -29,7 +43,11 @@ public class Material {
 			System.out.println("El material ya existe");
 		}
 	}
-	
+	/**
+	 * Establece cambio de la resistencia del material del objeto
+	 * @param name Nombre del material que se le asignara al objeto
+	 * @param endurance Resistencia del material que se le asignara al objeto
+	 */
 	public void changeEndurance(String name, int endurance) {			
 		if(Material.data.get(name) != null) {							
 			Material.data.remove(name);
@@ -37,24 +55,40 @@ public class Material {
 			this.endurance = endurance;
 		}
 	}
-	
+	/**
+	 * Método para regresar la informacion de material del objeto
+	 * @return Regresa la informacion de material del objeto
+	 */
 	public TreeMap<String, Integer> getMaterials(){
 		return Material.data;
 	}
-	
+	/**
+	 * Método para regresar el nombre de material del objeto
+	 * @return Regresa el nombre de material del objeto
+	 */
 	public String getName() {
 		return this.name;
 	}
-	
+	/**
+	 * Método para regresar la resitencia de material del objeto
+	 * @return Regresa la resistencia de material del objeto
+	 */
 	public int getEndurance() {
 		return this.endurance;
 	}
-	
+	/** 
+	 * Método para regresar cadena de texto de los atributos de material del objeto
+	 * @return Regresa cadena de texto de los atributos de material del objeto
+	 */
 	@Override
 	public String toString() {
 		return String.format("Material: %s    Endurance: %d", this.getName(), this.getEndurance());
 	}
-	
+	/** 
+	 * Método para regresar si los objetos de material son iguales e igualarlos
+	 * @return Regresa si los objetos de material son iguales
+	 * @return Regresa igualdad de los objetos de material
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if(!(o instanceof Material))
