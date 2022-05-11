437f35d55f4f5ef2cf61ba2a0b298a5e2e60c167
PP-rez <101601926+PP-rez@users.noreply.github.com>
2022-05-10 19:42:23 -0500

    documentacion hecha

--- EnduranceObject.java
+++ EnduranceObject.java
@@ -1,5 +1,9 @@
 package com.iteso.motor;
-
+/**
+ * Clase EnduranceObject de la aplicación
+ * @author José Pablo Vázquez Partida, Octavio Valdez Fonseca, José Eduardo Pérez Valenzuela
+ * @version 1.0
+ */
 public abstract class EnduranceObject {
 	public abstract void decrease();
 }
