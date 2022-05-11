ff9cf7e400aa6c711b1ccb3309753e89a75acbf5
PP-rez <101601926+PP-rez@users.noreply.github.com>
2022-05-10 19:40:37 -0500

    documentacion hecha

--- Direction.java
+++ Direction.java
@@ -1,5 +1,9 @@
 package com.iteso.motor;
-
+/**
+ * Clase Direction de la aplicación
+ * @author José Pablo Vázquez Partida, Octavio Valdez Fonseca, José Eduardo Pérez Valenzuela
+ * @version 1.0
+ */
 public enum Direction {
 	RIGHT,
 	LEFT,
