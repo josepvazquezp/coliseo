85425cb830bd4b753a4a911e79e2e8a30b3e91f8
José Pablo Vázquez Partida <74749686+josepvazquezp@users.noreply.github.com>
2022-05-11 18:05:12 -0500

    agregar l y h de cada uno de la uenumeracion

--- Armors.java
+++ Armors.java
@@ -4,24 +4,26 @@ import com.iteso.motor.Armor;
 import com.iteso.motor.Material;
 
 public enum Armors {
-	GOLD_HELMET(150, 50, true, "gold helmet", Materials.GOLD.getMaterial()), 
-	IRON_HELMET(100, 30, true, "iron helmet", Materials.IRON.getMaterial()),  
-	COPPER_HELMET(50, 20, true, "copper helmet", Materials.COPPER.getMaterial()), 
+	GOLD_HELMET(150, 50, true, "gold helmet", Materials.GOLD.getMaterial(), 0, 0), 
+	IRON_HELMET(100, 30, true, "iron helmet", Materials.IRON.getMaterial(), 0, 0),  
+	COPPER_HELMET(50, 20, true, "copper helmet", Materials.COPPER.getMaterial(), 0, 0), 
 	
-	GOLD_BREASTPLATE(300, 100, false, "gold breastplate", Materials.GOLD.getMaterial()), 
-	IRON_BREASTPLATE(200, 80, false, "gold breastplate", Materials.IRON.getMaterial()), 
-	COPPER_BREASTPLATE(100, 60, false, "gold breastplate", Materials.COPPER.getMaterial()),
+	GOLD_BREASTPLATE(300, 100, false, "gold breastplate", Materials.GOLD.getMaterial(), 0, 0), 
+	IRON_BREASTPLATE(200, 80, false, "gold breastplate", Materials.IRON.getMaterial(), 0, 0), 
+	COPPER_BREASTPLATE(100, 60, false, "gold breastplate", Materials.COPPER.getMaterial(), 0, 0),
 
-	IRON_SHIELD(100, 20, false, "shield", Materials.IRON.getMaterial());
+	IRON_SHIELD(100, 20, false, "shield", Materials.IRON.getMaterial(), 0, 0);
 	
 	private Armor armor = new Armor();
 	
-	private Armors(int defense, int weight, boolean helmet, String name, Material type) {
+	private Armors(int defense, int weight, boolean helmet, String name, Material type, int l, int h) {
 		armor.setDefense(defense);
 		armor.setWeight(weight);
 		armor.setHelmet(helmet);
 		armor.setName(name);
 		armor.setType(type);
+		armor.setLong(l);
+		armor.setHeight(h);
 	}
 	
 	public Armor getArmor() {
--- Weapons.java
+++ Weapons.java
@@ -4,33 +4,33 @@ import com.iteso.motor.Material;
 import com.iteso.motor.Weapon;
 
 public enum Weapons{
-	COPPER_MACE(50, 40, 1, 2, 0, 0, 100, false,"Copper Mace" , Materials.COPPER.getMaterial()),
-	IRON_MACE(55, 45, 1, 2, 0, 0, 150,false,"Iron Mace", Materials.IRON.getMaterial()),
-	GOLD_MACE(60, 50, 1, 2, 0, 0, 200,false,"Gold Mace", Materials.GOLD.getMaterial()),
+	COPPER_MACE(50, 40, 1, 2, 0, 0, 100, false,"Copper Mace" , Materials.COPPER.getMaterial(), 0, 0),
+	IRON_MACE(55, 45, 1, 2, 0, 0, 150,false,"Iron Mace", Materials.IRON.getMaterial(), 0, 0),
+	GOLD_MACE(60, 50, 1, 2, 0, 0, 200,false,"Gold Mace", Materials.GOLD.getMaterial(), 0, 0),
 	
-	COPPER_LONGAXE(40, 30, 1, 2, 0, 0, 100,false,"Copper Longaxe", Materials.COPPER.getMaterial()),
-	IRON_LONGAXE(45, 35, 1, 2, 0, 0, 150,false,"Iron Longaxe", Materials.IRON.getMaterial()),
-	GOLD_LONGAXE(50, 40, 1, 2, 0, 0, 200,false,"Gold Longaxe", Materials.GOLD.getMaterial()),
+	COPPER_LONGAXE(40, 30, 1, 2, 0, 0, 100,false,"Copper Longaxe", Materials.COPPER.getMaterial(), 0, 0),
+	IRON_LONGAXE(45, 35, 1, 2, 0, 0, 150,false,"Iron Longaxe", Materials.IRON.getMaterial(), 0, 0),
+	GOLD_LONGAXE(50, 40, 1, 2, 0, 0, 200,false,"Gold Longaxe", Materials.GOLD.getMaterial(), 0, 0),
 	
-	COPPER_AXE(10, 7, 1, 1, 0, 0, 100,true,"Copper Axe", Materials.COPPER.getMaterial()),
-	IRON_AXE(15, 9, 1, 1, 0, 0, 150,true,"Iron Axe", Materials.IRON.getMaterial()),
-	GOLD_AXE(20, 12, 1, 1, 0, 0, 200,true,"Gold Axe", Materials.GOLD.getMaterial()),
+	COPPER_AXE(10, 7, 1, 1, 0, 0, 100,true,"Copper Axe", Materials.COPPER.getMaterial(), 0, 0),
+	IRON_AXE(15, 9, 1, 1, 0, 0, 150,true,"Iron Axe", Materials.IRON.getMaterial(), 0, 0),
+	GOLD_AXE(20, 12, 1, 1, 0, 0, 200,true,"Gold Axe", Materials.GOLD.getMaterial(), 0, 0),
 	
-	COPPER_SWORD(15, 10, 1, 1, 0, 0, 100,false,"Copper Sword", Materials.COPPER.getMaterial()),
-	IRON_SWORD(20, 12, 1, 1, 0, 0, 150,false,"Iron Sword", Materials.IRON.getMaterial()),
-	GOLD_SWORD(25, 15, 1, 1, 0, 0, 200,false,"Gold Sword", Materials.GOLD.getMaterial()),
+	COPPER_SWORD(15, 10, 1, 1, 0, 0, 100,false,"Copper Sword", Materials.COPPER.getMaterial(), 0, 0),
+	IRON_SWORD(20, 12, 1, 1, 0, 0, 150,false,"Iron Sword", Materials.IRON.getMaterial(), 0, 0),
+	GOLD_SWORD(25, 15, 1, 1, 0, 0, 200,false,"Gold Sword", Materials.GOLD.getMaterial(), 0, 0),
 	
-	COPPER_KUNAI(5, 2, 1, 1, 0, 0, 100,true,"Copper Kunai", Materials.COPPER.getMaterial()),
-	IRON_KUNAI(10, 5, 1, 1, 0, 0, 150,true, "Iron Kunai", Materials.IRON.getMaterial()),
-	GOLD_KUNAI(13, 7, 1, 1, 0, 0, 200,true,"Gold Kunai", Materials.GOLD.getMaterial()),
+	COPPER_KUNAI(5, 2, 1, 1, 0, 0, 100,true,"Copper Kunai", Materials.COPPER.getMaterial(), 0, 0),
+	IRON_KUNAI(10, 5, 1, 1, 0, 0, 150,true, "Iron Kunai", Materials.IRON.getMaterial(), 0, 0),
+	GOLD_KUNAI(13, 7, 1, 1, 0, 0, 200,true,"Gold Kunai", Materials.GOLD.getMaterial(), 0, 0),
 	
-	IRON_SHIELD(15, 20, 1, 1, 0, 0, 150,true,"Iron Shield", Materials.IRON.getMaterial()),
+	IRON_SHIELD(15, 20, 1, 1, 0, 0, 150,true,"Iron Shield", Materials.IRON.getMaterial(), 0, 0),
 	
-	FIST(10, 0, 1, 1, 0, 0, 100, false,"Fist", Materials.FLESH.getMaterial());
+	FIST(10, 0, 1, 1, 0, 0, 100, false,"Fist", Materials.FLESH.getMaterial(), 0, 0);
 	
 	private Weapon w = new Weapon();
 
-	Weapons(int pow, int weight, int distance, int capacity, int x, int y, int endurance, boolean flyable,String name, Material material) {
+	Weapons(int pow, int weight, int distance, int capacity, int x, int y, int endurance, boolean flyable,String name, Material material, int l, int h) {
 		w.setPow(pow);
 		w.setWeight(weight);
 		w.setDistance(distance);
@@ -41,6 +41,8 @@ public enum Weapons{
 		w.setName(name);
 		w.setType(material);
 		w.setEndurance(endurance);
+		w.setLong(l);
+		w.setHeight(h);
 	}
 	
 	public Weapon getWeapon() {
