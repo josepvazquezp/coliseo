92128f83b644f0584423db7ae92de68d12c1a922
OctavioValdez <90404916+OctavioValdez@users.noreply.github.com>
2022-05-13 02:07:29 -0500

    Long y height en armor

--- Armors.java
+++ Armors.java
@@ -4,15 +4,15 @@ import com.iteso.motor.Armor;
 import com.iteso.motor.Material;
 
 public enum Armors {
-	GOLD_HELMET(150, 50, true, "gold helmet", Materials.GOLD.getMaterial(), 0, 0), 
-	IRON_HELMET(100, 30, true, "iron helmet", Materials.IRON.getMaterial(), 0, 0),  
-	COPPER_HELMET(50, 20, true, "copper helmet", Materials.COPPER.getMaterial(), 0, 0), 
+	GOLD_HELMET(150, 50, true, "gold helmet", Materials.GOLD.getMaterial(), 50, 25), 
+	IRON_HELMET(100, 30, true, "iron helmet", Materials.IRON.getMaterial(), 50, 25),  
+	COPPER_HELMET(50, 20, true, "copper helmet", Materials.COPPER.getMaterial(), 50, 25), 
 	
-	GOLD_BREASTPLATE(300, 100, false, "gold breastplate", Materials.GOLD.getMaterial(), 0, 0), 
-	IRON_BREASTPLATE(200, 80, false, "gold breastplate", Materials.IRON.getMaterial(), 0, 0), 
-	COPPER_BREASTPLATE(100, 60, false, "gold breastplate", Materials.COPPER.getMaterial(), 0, 0),
+	GOLD_BREASTPLATE(300, 100, false, "gold breastplate", Materials.GOLD.getMaterial(), 100, 50), 
+	IRON_BREASTPLATE(200, 80, false, "gold breastplate", Materials.IRON.getMaterial(), 100, 50), 
+	COPPER_BREASTPLATE(100, 60, false, "gold breastplate", Materials.COPPER.getMaterial(), 100, 50),
 
-	IRON_SHIELD(100, 20, false, "shield", Materials.IRON.getMaterial(), 0, 0);
+	IRON_SHIELD(100, 20, false, "shield", Materials.IRON.getMaterial(), 100, 100);
 	
 	private Armor armor = new Armor();
 	
