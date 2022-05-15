c5cc75d37959fbc1c9dc61087f712a994da8c13d
José Pablo Vázquez Partida <74749686+josepvazquezp@users.noreply.github.com>
2022-05-15 14:05:46 -0500

    update atributes

--- Armors.java
+++ Armors.java
@@ -4,15 +4,15 @@ import com.iteso.motor.Armor;
 import com.iteso.motor.Material;
 
 public enum Armors {
-	GOLD_HELMET(150, 50, true, "gold helmet", Materials.GOLD.getMaterial(), 50, 25), 
-	IRON_HELMET(100, 30, true, "iron helmet", Materials.IRON.getMaterial(), 50, 25),  
-	COPPER_HELMET(50, 20, true, "copper helmet", Materials.COPPER.getMaterial(), 50, 25), 
+	GOLD_HELMET(150, 50, true, "Gold Helmet", Materials.GOLD.getMaterial(), 50, 25), 
+	IRON_HELMET(100, 30, true, "Iron Helmet", Materials.IRON.getMaterial(), 50, 25),  
+	COPPER_HELMET(50, 20, true, "Copper Helmet", Materials.COPPER.getMaterial(), 50, 25), 
 	
-	GOLD_BREASTPLATE(300, 100, false, "gold breastplate", Materials.GOLD.getMaterial(), 100, 50), 
-	IRON_BREASTPLATE(200, 80, false, "gold breastplate", Materials.IRON.getMaterial(), 100, 50), 
-	COPPER_BREASTPLATE(100, 60, false, "gold breastplate", Materials.COPPER.getMaterial(), 100, 50),
+	GOLD_BREASTPLATE(300, 100, false, "Gold Breastplate", Materials.GOLD.getMaterial(), 100, 50), 
+	IRON_BREASTPLATE(200, 80, false, "Gold Breastplate", Materials.IRON.getMaterial(), 100, 50), 
+	COPPER_BREASTPLATE(100, 60, false, "Gold Breastplate", Materials.COPPER.getMaterial(), 100, 50),
 
-	IRON_SHIELD(100, 20, false, "shield", Materials.IRON.getMaterial(), 80, 60);
+	IRON_SHIELD(100, 20, false, "Shield", Materials.IRON.getMaterial(), 80, 60);
 	
 	private Armor armor = new Armor();
 	
--- Weapons.java
+++ Weapons.java
@@ -8,25 +8,25 @@ public enum Weapons{
 	IRON_MACE(55, 45, 1, 2, 0, 0, 150,false,"Iron Mace", Materials.IRON.getMaterial(), 100, 75),
 	GOLD_MACE(60, 50, 1, 2, 0, 0, 200,false,"Gold Mace", Materials.GOLD.getMaterial(), 100, 75),
 	
-	COPPER_LONGAXE(40, 30, 1, 2, 0, 0, 100,false,"Copper Longaxe", Materials.COPPER.getMaterial(), 90, 70),
-	IRON_LONGAXE(45, 35, 1, 2, 0, 0, 150,false,"Iron Longaxe", Materials.IRON.getMaterial(), 90, 70),
-	GOLD_LONGAXE(50, 40, 1, 2, 0, 0, 200,false,"Gold Longaxe", Materials.GOLD.getMaterial(), 90, 70),
+	COPPER_LONG_AXE(40, 30, 1, 2, 0, 0, 100,false,"Copper Longaxe", Materials.COPPER.getMaterial(), 90, 70),
+	IRON_LONG_AXE(45, 35, 1, 2, 0, 0, 150,false,"Iron Longaxe", Materials.IRON.getMaterial(), 90, 70),
+	GOLD_LONG_AXE(50, 40, 1, 2, 0, 0, 200,false,"Gold Longaxe", Materials.GOLD.getMaterial(), 90, 70),
 	
-	COPPER_AXE(10, 7, 1, 1, 0, 0, 100,true,"Copper Axe", Materials.COPPER.getMaterial(), 35, 20),
-	IRON_AXE(15, 9, 1, 1, 0, 0, 150,true,"Iron Axe", Materials.IRON.getMaterial(), 35, 20),
-	GOLD_AXE(20, 12, 1, 1, 0, 0, 200,true,"Gold Axe", Materials.GOLD.getMaterial(), 35, 20),
+	COPPER_AXE(18, 7, 1, 1, 0, 0, 100,true,"Copper Axe", Materials.COPPER.getMaterial(), 35, 20),
+	IRON_AXE(22, 9, 1, 1, 0, 0, 150,true,"Iron Axe", Materials.IRON.getMaterial(), 35, 20),
+	GOLD_AXE(26, 12, 1, 1, 0, 0, 200,true,"Gold Axe", Materials.GOLD.getMaterial(), 35, 20),
 	
-	COPPER_SWORD(15, 10, 1, 1, 0, 0, 100,false,"Copper Sword", Materials.COPPER.getMaterial(), 50, 45),
-	IRON_SWORD(20, 12, 1, 1, 0, 0, 150,false,"Iron Sword", Materials.IRON.getMaterial(), 50, 45),
-	GOLD_SWORD(25, 15, 1, 1, 0, 0, 200,false,"Gold Sword", Materials.GOLD.getMaterial(), 50, 45),
+	COPPER_SWORD(20, 10, 1, 1, 0, 0, 100,false,"Copper Sword", Materials.COPPER.getMaterial(), 50, 45),
+	IRON_SWORD(25, 12, 1, 1, 0, 0, 150,false,"Iron Sword", Materials.IRON.getMaterial(), 50, 45),
+	GOLD_SWORD(30, 15, 1, 1, 0, 0, 200,false,"Gold Sword", Materials.GOLD.getMaterial(), 50, 45),
 	
-	COPPER_KUNAI(5, 2, 1, 1, 0, 0, 100,true,"Copper Kunai", Materials.COPPER.getMaterial(), 20, 10),
-	IRON_KUNAI(10, 5, 1, 1, 0, 0, 150,true, "Iron Kunai", Materials.IRON.getMaterial(), 20, 10),
-	GOLD_KUNAI(13, 7, 1, 1, 0, 0, 200,true,"Gold Kunai", Materials.GOLD.getMaterial(), 20, 10),
+	COPPER_KUNAI(15, 2, 1, 1, 0, 0, 100,true,"Copper Kunai", Materials.COPPER.getMaterial(), 20, 10),
+	IRON_KUNAI(20, 5, 1, 1, 0, 0, 150,true, "Iron Kunai", Materials.IRON.getMaterial(), 20, 10),
+	GOLD_KUNAI(23, 7, 1, 1, 0, 0, 200,true,"Gold Kunai", Materials.GOLD.getMaterial(), 20, 10),
 	
 	IRON_SHIELD(15, 20, 1, 1, 0, 0, 150,true,"Iron Shield", Materials.IRON.getMaterial(), 80, 60),
 	
-	FIST(10, 0, 1, 1, 0, 0, 100, false,"Fist", Materials.FLESH.getMaterial(), 10, 10);
+	FIST(10, 0, 1, 0, 0, 0, 100, false,"Fist", Materials.FLESH.getMaterial(), 10, 10);
 	
 	private Weapon w = new Weapon();
 
